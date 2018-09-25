0480934012c2ab02801b326b199f4aeec86ad2c2
Chloé Dequeker <noreply@nelyah.eu>
2018-09-25 15:26:06 +0200

    Added all the code to an __init__ method.
    Also fixed an issue where the '.' char wouldn't be taken into account
    when considering extensions.

--- ls_colors.py
+++ ls_colors.py
@@ -6,73 +6,78 @@ from os import getenv
 from subprocess import check_output, CalledProcessError
 
 
-def get_default_lscolors():
-    """Returns the default value for LS_COLORS
-    as parsed from the `dircolors` command
-    """
-    ls_colors = check_output('dircolors')
-    ls_colors = ls_colors.splitlines()[0].decode('UTF-8').split("'")[1]
-    return ls_colors
+class ls_colors(ColorScheme):
+    def __init__(self):
+        super(ColorScheme, self).__init__()
+        try:
+            self.ls_colors = getenv('LS_COLORS',
+                                    self.get_default_lscolors()).split(':')
+        except (CalledProcessError, FileNotFoundError):
+            self.ls_colors = []
 
+        # Gets all the keys corresponding to extensions
+        self.ls_colors_extensions = [
+            k.split('=')[0] for k in self.ls_colors if k != ''
+        ]
+        self.ls_colors_extensions = [
+            '.' + k.split('*.')[1] for k in self.ls_colors_extensions
+            if '*.' in k
+        ]
 
-try:
-    ls_colors = getenv('LS_COLORS', get_default_lscolors()).split(':')
-except (CalledProcessError, FileNotFoundError):
-    ls_colors = []
+        # Add the key names to ranger context keys
+        for key in self.ls_colors_extensions:
+            ranger.gui.context.CONTEXT_KEYS.append(key)
+            setattr(ranger.gui.context.Context, key, False)
 
-# Gets all the keys corresponding to extensions
-ls_colors_keys = [k.split('=')[0] for k in ls_colors if k != '']
-ls_colors_keys = [k.split('*.')[1] for k in ls_colors_keys if '*.' in k]
+        self.OLD_HOOK_BEFORE_DRAWING = ranger.gui.widgets.browsercolumn.hook_before_drawing
 
-# Add the key names to ranger context keys
-for key in ls_colors_keys:
-    ranger.gui.context.CONTEXT_KEYS.append(key)
-    setattr(ranger.gui.context.Context, key, False)
+        ranger.gui.widgets.browsercolumn.hook_before_drawing = self.new_hook_before_drawing
 
-OLD_HOOK_BEFORE_DRAWING = ranger.gui.widgets.browsercolumn.hook_before_drawing
+        self.ls_colors_keys = [k.split('=') for k in self.ls_colors if k != '']
+        self.tup_ls_colors = []
 
+        # Not considering file extensions
+        # The order of these two block matters, as extensions colouring should
+        # take precedence over file type
+        for key in [k for k in self.ls_colors_keys if '.*' not in k]:
+            if key[0] == 'fi':
+                self.tup_ls_colors += [('file', key[1])]
 
-def new_hook_before_drawing(fsobject, color_list):
-    for key in ls_colors_keys:
-        if fsobject.basename.endswith(key):
-            color_list.append(key)
+        # Considering files extensions
+        self.tup_ls_colors += [('.' + k[0].split('*.')[1], k[1])
+                               for k in self.ls_colors_keys if '*.' in k[0]]
 
-    return OLD_HOOK_BEFORE_DRAWING(fsobject, color_list)
+        for key in [k for k in self.ls_colors_keys if '.*' not in k]:
+            if key[0] == 'ex':
+                self.tup_ls_colors += [('executable', key[1])]
+            elif key[0] == 'pi':
+                self.tup_ls_colors += [('fifo', key[1])]
+            elif key[0] == 'ln':
+                self.tup_ls_colors += [('link', key[1])]
+            elif key[0] == 'bd' or key[0] == 'cd':
+                self.tup_ls_colors += [('device', key[1])]
+            elif key[0] == 'so':
+                self.tup_ls_colors += [('socket', key[1])]
+            elif key[0] == 'di':
+                self.tup_ls_colors += [('directory', key[1])]
 
+        self.progress_bar_color = 1
+        print(self.ls_colors_keys)
 
-ranger.gui.widgets.browsercolumn.hook_before_drawing = new_hook_before_drawing
+    def new_hook_before_drawing(self, fsobject, color_list):
+        for key in self.ls_colors_extensions:
+            if fsobject.basename.endswith(key):
+                color_list.append(key)
 
+        return self.OLD_HOOK_BEFORE_DRAWING(fsobject, color_list)
 
-class base(ColorScheme):
-    progress_bar_color = 1
-
-    ls_colors_keys = [k.split('=') for k in ls_colors if k != '']
-    tup_ls_colors = []
-
-    # Not considering file extensions
-    # The order of these two block matters, as extensions colouring should take
-    # precedence over file type
-    for key in [k for k in ls_colors_keys if '.*' not in k]:
-        if key[0] == 'fi':
-            tup_ls_colors += [('file', key[1])]
-
-    # Considering files extensions
-    tup_ls_colors += [(k[0].split('*.')[1], k[1]) for k in ls_colors_keys
-                      if '*.' in k[0]]
-
-    for key in [k for k in ls_colors_keys if '.*' not in k]:
-        if key[0] == 'ex':
-            tup_ls_colors += [('executable', key[1])]
-        elif key[0] == 'pi':
-            tup_ls_colors += [('fifo', key[1])]
-        elif key[0] == 'ln':
-            tup_ls_colors += [('link', key[1])]
-        elif key[0] == 'bd' or key[0] == 'cd':
-            tup_ls_colors += [('device', key[1])]
-        elif key[0] == 'so':
-            tup_ls_colors += [('socket', key[1])]
-        elif key[0] == 'di':
-            tup_ls_colors += [('directory', key[1])]
+    def get_default_lscolors(self):
+        """Returns the default value for LS_COLORS
+        as parsed from the `dircolors` command
+        """
+        ls_colors = check_output('dircolors')
+        ls_colors = ls_colors.splitlines()[0].decode('UTF-8').split("'")[1]
+        return ls_colors
 
     def get_attr_from_lscolors(self, attribute_list):
         return_attr = 0
@@ -152,14 +157,14 @@ class base(ColorScheme):
                 colour16_fg, colour16_bg = None, None
                 for colour_val in t_attributes:
                     colour_val = int(colour_val)
-                    # This is an attribute
-                    if (colour_val >= 30
-                            and colour_val <= 37):  # Basic colours
+                    # Basic colours for foreground
+                    if (colour_val >= 30 and colour_val <= 37):
                         colour16_fg = colour_val - 30
                     # eight more basic colours
                     elif (colour_val >= 90 and colour_val <= 97):
                         colour16_fg = colour_val - 82
 
+                    # Basic colours for background
                     elif (colour_val >= 40 and colour_val <= 47):
                         colour16_bg = colour_val
                     # eight more basic colours
